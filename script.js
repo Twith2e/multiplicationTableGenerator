01a98a73903a315aa313d2ce696ac6aecb24d1dd
twith2e <abdulbasitadebajo1619@gmail.com>
2024-06-20 08:26:49 +0100

    minor upgrades

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ function showTable() {
   endInput = document.getElementById("end");
   displayNum = document.getElementById("show-multiples");
   displayNum.innerHTML = "";
+  myHead = document.getElementById("head");
+  myHead.style.display = "none";
 
   if (startInput.value != "" && endInput.value != "") {
-    if (endInput.value >= startInput.value) {
+    if (endInput.value > startInput.value) {
       myHead = document.getElementById("head");
       myHead.style.display = "block";
-      firstNum = document.getElementById("first-num");
-      firstNum.innerHTML = startInput.value;
-      secondNum = document.getElementById("second-num");
-      secondNum.innerHTML = endInput.value;
+      myHead.innerHTML = `${startInput.value} to ${endInput.value} multiples table`;
       for (
         begin = Number(startInput.value);
         begin <= Number(endInput.value);
@@ -25,12 +24,40 @@ function showTable() {
       }
       startInput.value = "";
       endInput.value = "";
+      startInput.style.borderColor = "green";
+      endInput.style.borderColor = "green";
+      document.querySelector(".error-msg").style.display = "none";
+    } else if (endInput.value === startInput.value) {
+      myHead = document.getElementById("head");
+      myHead.style.display = "block";
+      myHead.innerHTML = `${startInput.value} multiples table`;
+      for (
+        begin = Number(startInput.value);
+        begin <= Number(endInput.value);
+        begin++
+      ) {
+        numDiv = document.createElement("div");
+        for (i = 1; i <= 12; i++) {
+          numDiv.innerHTML += `<p>${begin} x ${i} = ${begin * i}</p>`;
+        }
+        displayNum.appendChild(numDiv);
+      }
+      startInput.value = "";
+      endInput.value = "";
+      endInput.style.borderColor = "green";
+      startInput.style.borderColor = "green";
+      document.querySelector(".error-msg").style.display = "none";
     } else {
-      alert("The second value has to higher than the first value");
+      document.querySelector(".error-msg").style.display = "block";
+      startInput.style.borderColor = "green";
+      endInput.style.borderColor = "red";
       myHead.style.display = "none";
     }
   } else {
-    alert("Kindly check the input fields again");
+    // document.getElementById("error-messages").style.display = "block";
+    // startInput.style.borderColor = "red";
+    // endInput.style.borderColor = "red";
+    alert("Input Fields are empty!!!");
     myHead.style.display = "none";
   }
 }
